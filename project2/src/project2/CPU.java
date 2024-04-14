0215a35f81809251d05f58c0afe60dcadca0a3c7
benbrown331 <68621257+benbrown331@users.noreply.github.com>
2024-04-14 19:54:39 -0400

    View Implementation

--- project2/src/project2/CPU.java
+++ project2/src/project2/CPU.java
@@ -16,7 +16,7 @@ public final class CPU{
 
 
         ScoreBoard view = new ScoreBoard();
-        Controller controller = new Controller(view, "output.dat");
+        Controller controller = new Controller(view, "C:\\Users\\Benjamin\\IdeaProjects\\project2\\project2\\src\\project2\\output.dat");
         controller.runProcessor();
     }catch (IOException e) {
         System.err.println("Failed to initialize the Controller: " + e.getMessage());
